579e39f17766274a09f593630b8f1ebfd86fd370
SevSudERD <161018073+SevSudERD@users.noreply.github.com>
2024-05-22 21:29:45 +0300

    mern full stack
    app

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const { connectToMongoDB} = require("./database");
+
+const app = express();
+app.use(express.json());
+
+const router = require("./routes");
+
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
+
+async function startServer() {
+  await connectToMongoDB();
+  app.listen(port, () =>{
+    console.log(`Server is listening on http://localhost:${port}`);
+  
+  });
+}
+startServer();
+
